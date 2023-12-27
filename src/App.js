b0dd246fa9608490f3456e0842fe3f85fa265f3b
harish <harishnagubadi9190@gmail.com>
2023-12-27 12:13:22 +0530

    theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./App.css"
 import ToDoList from "./ToDo/ToDoList"
+import dark from "./icons/dark.png"
+import light from "./icons/light.png"
+
 
 export default function ToDo() {
 
   const [task,setTask] = React.useState("")
   const [toDo,setToDo] = React.useState([])
+  const [on,setOn] = React.useState(false)
 
   function handleChange(e)
   {
@@ -24,16 +28,28 @@ export default function ToDo() {
     const newTodo = toDo.filter((todo,index) => index !== indexVal);
     setToDo(newTodo);
   }
- 
+
+function handleMode(){
+  setOn(!on)
+}
+
+  const style ={
+    backgroundColor : on ?  "black": "white",
+    color: on ? "white":"black"
+  }
+
+  
 
   return (
-    <div className="container">
+    <div className="container" style={style}>
 
+   <img className="theme" src={on ? light : dark} onClick={handleMode} 
+        alt="dark" height={on?30:32} width={31}/>
     <h3 className="title">Todo Management App</h3>
     
     <form className="input" onSubmit={submitHandler}>
 
-    <input type="text" value={task} 
+    <input type="text" value={task} style={style}
     onChange={handleChange}/>
     <button className="add" disabled={task.length === 0}>Add</button>
 
